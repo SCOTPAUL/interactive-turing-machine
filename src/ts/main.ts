fa6404ba259c64fd0728c26bc66dfae4d577efb6
Paul Cowie <paul.cowie@ntlworld.com>
2016-09-24 18:10:40 +0100

    Remove old deps

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,6 +4,7 @@ import * as state_machine from './state-machine';
 
 function init(){
   var machine = new TuringMachine("1");
+  var initial_node = machine.addState(0);
 }
 
 document.addEventListener("DOMContentLoaded", function(event){
